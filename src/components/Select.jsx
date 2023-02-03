9824ea0d24ae7c02480b6890be9a333c105b5195
Felizxfell <felizxhgf@gmial.com>
2023-02-03 15:14:30 -0600

    select corregido

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -4,14 +4,18 @@ const URLAPI = "https://localhost:7121/api/Puesto";
 
 export default function Select({ puestoId }) {
   const [puestos, setPuestos] = useState([]);
-
-  console.log(puestoId);
+  const [value, setValue] = useState(puestoId ? puestoId : 0);
 
   useEffect(() => {
     fetch(URLAPI)
     .then((resp) => resp.json())
     .then((data) => setPuestos(data));    
   }, [puestoId]);
+
+
+  const handleChange = e => {
+    setValue(e.target.value)
+  }
 
   return (
     <>
@@ -20,7 +24,8 @@ export default function Select({ puestoId }) {
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
       >                
         <select
-          defaultValue={puestoId ? puestoId : 0}
+          value={value}
+          onChange={handleChange}
           id="puesto"
           name="puesto"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
